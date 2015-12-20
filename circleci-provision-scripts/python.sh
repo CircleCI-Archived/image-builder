034e12345ea8ad22174f9493483a24f4f2f930e6
Kim, Hirokuni <yangkookkim@gmail.com>
2015-12-21 01:15:38 +0900

    wip

--- circleci-provision-scripts/python.sh
+++ circleci-provision-scripts/python.sh
@@ -1,20 +1,20 @@
 #!/bin/bash
 
+function switch_python() {
+    VERSION=$1
+    RC_FILE=/home/$CIRCLECI_USER/.circlerc
+
+    echo ">>> Using Python $VERSION"
+    
+    echo "export PATH=/opt/circleci/python/${VERSION}/bin:$PATH" >> $RC_FILE
+    
+    source $RC_FILE
+}
+
 function install_python() {
     VERSION=$1
 
     sudo apt-get install circleci-python${VERSION}=0.0.1
-    
     chown -R $CIRCLECI_USER:$CIRCLECI_USER /opt/circleci/python/$VERSION
-    
-    (cat <<'EOF'
-
-echo "export PATH=/opt/circleci/python/${VERSION}/bin:$PATH" >> ~/.circlerc
-
-source  ~/.circlerc
-pip install -U virtualenv
-pip install -U nose
-pip install -U pep8
-EOF
-    ) | as_user VERSION=$VERSION bash    
+    switch_python $VERSION
 }
--- circleci-provision-scripts/ruby.sh
+++ circleci-provision-scripts/ruby.sh
@@ -1,20 +1,21 @@
 #!/bin/bash
 
+function switch_ruby() {
+    VERSION=$1
+    RC_FILE=/home/$CIRCLECI_USER/.circlerc
+
+    echo ">>> Using Ruby $VERSION"
+    
+    echo "export PATH=/opt/circleci/ruby/${VERSION}/bin:$PATH" >> $RC_FILE
+    echo "export RUBYLIB=/opt/circleci/ruby/${VERSION}/lib/ruby/2.2.0:/opt/circleci/ruby/${VERSION}/lib/ruby/2.2.0/x86_64-linux" >> $RC_FILE
+    
+    source $RC_FILE
+}
+
 function install_ruby() {
     VERSION=$1
-
+    
     sudo apt-get install circleci-ruby${VERSION}=0.0.1
     chown -R $CIRCLECI_USER:$CIRCLECI_USER /opt/circleci/ruby/$VERSION
-    
-    (cat <<'EOF'
-
-echo "export PATH=/opt/circleci/ruby/${VERSION}/bin:$PATH" >> ~/.circlerc
-echo "export RUBYLIB=/opt/circleci/ruby/${VERSION}/lib/ruby/2.2.0:/opt/circleci/ruby/${VERSION}/lib/ruby/2.2.0/x86_64-linux" >> ~/.circlerc
-
-source  ~/.circlerc
-gem install bundler
-
-EOF
-    ) | as_user VERSION=$VERSION bash
-    
+    switch_ruby $VERSION
 }
