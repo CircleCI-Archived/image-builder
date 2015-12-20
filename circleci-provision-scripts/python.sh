ce778cb7d26e56c52d3b2e5034173b8df4f544e4
Kim, Hirokuni <yangkookkim@gmail.com>
2015-12-20 21:34:23 +0900

    wip

--- circleci-provision-scripts/python.sh
+++ circleci-provision-scripts/python.sh
@@ -1,53 +1,20 @@
 #!/bin/bash
 
-function install_pyenv() {
-    echo '>>> Installing Python'
+function install_python() {
+    VERSION=$1
 
-    # FROM https://github.com/yyuu/pyenv/wiki/Common-build-problems
-    sudo apt-get install -y make build-essential libssl-dev zlib1g-dev libbz2-dev \
-	    libreadline-dev libsqlite3-dev wget curl llvm libncurses5-dev
-
-    echo 'Installing pyenv'
+    sudo apt-get install circleci-python${VERSION}=0.0.1
+    
+    chown -R $CIRCLECI_USER:$CIRCLECI_USER /opt/circleci/python/$VERSION
+    
     (cat <<'EOF'
-set -e
-cd ~
-[[ -e .pyenv ]] || git clone https://github.com/yyuu/pyenv.git .pyenv
-echo 'export PATH=~/.pyenv/bin:$PATH' >> ~/.circlerc
-echo 'eval "$(pyenv init -)"' >> ~/.circlerc
-EOF
-    ) | as_user bash
-}
 
-function install_python_version() {
-    PYTHON_VERSION=$1
-    (cat <<'EOF'
-set -e
-source ~/.circlerc
-pyenv install $PYTHON_VERSION
-pyenv global $PYTHON_VERSION
-pyenv rehash
+echo "export PATH=/opt/circleci/python/${VERSION}/bin:$PATH" >> ~/.circlerc
 
+source  ~/.circlerc
 pip install -U virtualenv
 pip install -U nose
 pip install -U pep8
-
 EOF
-    ) | as_user PYTHON_VERSION=$PYTHON_VERSION bash
-}
-
-function set_python_default() {
-    PYTHON_VERSION=$1
-    (cat <<'EOF'
-set -e
-source ~/.circlerc
-pyenv global $PYTHON_VERSION
-pyenv rehash
-EOF
-    ) | as_user PYTHON_VERSION=$PYTHON_VERSION bash
-}
-
-function install_python() {
-    VERSION=$1
-    [[ -e $CIRCLECI_HOME/.pyenv ]] || install_pyenv
-    install_python_version $1
+    ) | as_user VERSION=$VERSION bash    
 }
--- circleci-provision-scripts/ruby.sh
+++ circleci-provision-scripts/ruby.sh
@@ -1,67 +1,20 @@
 #!/bin/bash
 
-function install_rvm() {
-    echo '>>> Installing RVM and Ruby'
-
-    as_user gpg --keyserver hkp://keys.gnupg.net --recv-keys 409B6B1796C275462A1703113804BB82D39DC0E3
-    curl -sSL https://get.rvm.io | as_user bash -s stable --ruby
-
-    ln -sf ${CIRCLECI_HOME}/.rvm /usr/local/rvm
-
-    echo '[[ -s "$HOME/.rvm/scripts/rvm" ]] && . "$HOME/.rvm/scripts/rvm" # Load RVM function' | as_user tee -a ${CIRCLECI_HOME}/.circlerc
-
-    # Setting up user rmvrc
-
-    (cat <<'EOF'
-export rvm_gemset_create_on_use_flag=1
-export rvm_install_on_use_flag=1
-export rvm_trust_rvmrcs_flag=1
-export rvm_verify_downloads_flag=1
-EOF
-    ) | as_user tee ${CIRCLECI_HOME}/.rvmrc
-
-    # Setting up default gemrc
-
-    (cat <<'EOF'
-:sources:
-- https://rubygems.org
-gem:  --no-ri --no-rdoc
-EOF
-    ) | as_user tee ${CIRCLECI_HOME}/.gemrc
-
-    (cat <<'EOF'
-source ~/.circlerc
-rvm rvmrc warning ignore allGemfiles
-EOF
-    ) | as_user bash
-}
-
-
-function install_ruby_version() {
-    RUBY_VERSION=$1
-    RUBYGEMS_MAJOR_RUBY_VERSION=${2:-2}
-    (cat <<'EOF'
-
-set -e
-
-echo Installing Ruby version: $RUBY_VERSION
-source ~/.circlerc
-
-rvm use $RUBY_VERSION
-
-# TODO: Avoid this for jruby
-rvm rubygems latest-${RUBYGEMS_MAJOR_RUBY_VERSION}
-rvm @global do gem install bundler -v 1.9.5
-
-# For projects without gemfiles
-rvm @global do gem install rspec
-
-EOF
-    ) | as_user RUBY_VERSION=$RUBY_VERSION RUBYGEMS_MAJOR_RUBY_VERSION=$RUBYGEMS_MAJOR_RUBY_VERSION bash
-}
-
 function install_ruby() {
     VERSION=$1
-    [[ -e $CIRCLECI_HOME/.rvm ]] || install_rvm
-    install_ruby_version $1
+
+    sudo apt-get install circleci-ruby${VERSION}=0.0.1
+    chown -R $CIRCLECI_USER:$CIRCLECI_USER /opt/circleci/ruby/$VERSION
+    
+    (cat <<'EOF'
+
+echo "export PATH=/opt/circleci/ruby/${VERSION}/bin:$PATH" >> ~/.circlerc
+echo "export RUBYLIB=/opt/circleci/ruby/${VERSION}/lib/ruby/2.2.0:/opt/circleci/ruby/${VERSION}/lib/ruby/2.2.0/x86_64-linux" >> ~/.circlerc
+
+source  ~/.circlerc
+gem install bundler
+
+EOF
+    ) | as_user VERSION=$VERSION bash
+    
 }
