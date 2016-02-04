9bc4eaac1563ecc9500ab88e41b5bdfc84c3ecc1
Lizzie Dixon <_@lizzie.io>
2016-02-03 17:26:25 -0800

    install specific firefox versions.

--- circleci-provision-scripts/firefox.sh
+++ circleci-provision-scripts/firefox.sh
@@ -5,3 +5,12 @@ function install_firefox() {
 
     apt-get install -y firefox
 }
+
+function install_firefox_version() {
+    VERSION="$1"
+    echo ">>> Installing Firefox $VERSION"
+    curl -L -o "/tmp/firefox-$VERSION.tar.bz2" "https://download-installer.cdn.mozilla.net/pub/firefox/releases/$VERSION/linux-i686/en-US/firefox-$VERSION.tar.bz2"
+    tar -jxf "/tmp/firefox-$VERSION.tar.bz2" -C "/tmp/"
+    sudo mv "/tmp/firefox/firefox" "/opt/firefox-$VERSION"
+    sudo ln -sf "/opt/firefox-$VERSION/firefox" "/usr/bin/"
+}
