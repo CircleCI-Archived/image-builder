fe6d40966f0033bb7b2c30fd4fd4d7ef7051b4a0
Kim, Hirokuni <yangkookkim@gmail.com>
2016-02-22 12:21:50 +0900

    Update PhantomJS to 2.1.1

--- circleci-provision-scripts/phantomjs.sh
+++ circleci-provision-scripts/phantomjs.sh
@@ -3,7 +3,5 @@
 function install_phantomjs() {
     echo '>>> Installing PhantomJS'
 
-    curl --output /home/ubuntu/bin/phantomjs-2.0.1-linux-x86_64-dynamic https://s3.amazonaws.com/circle-support-bucket/phantomjs/phantomjs-2.0.1-linux-x86_64-dynamic
-    chmod a+x /home/ubuntu/bin/phantomjs-2.0.1-linux-x86_64-dynamic
-    sudo ln -s --force /home/ubuntu/bin/phantomjs-2.0.1-linux-x86_64-dynamic /usr/local/bin/phantomjs
+    curl --output /usr/local/bin/phantomjs https://s3.amazonaws.com/circle-downloads/phantomjs-2.1.1
 }
