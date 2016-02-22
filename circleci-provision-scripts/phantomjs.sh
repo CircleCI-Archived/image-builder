e211623175ad0073a10daaaf813b263b2823b92f
Kim, Hirokuni <yangkookkim@gmail.com>
2016-02-22 14:49:59 +0900

    Add exec perm to Phantomjs

--- circleci-provision-scripts/phantomjs.sh
+++ circleci-provision-scripts/phantomjs.sh
@@ -3,5 +3,8 @@
 function install_phantomjs() {
     echo '>>> Installing PhantomJS'
 
-    curl --output /usr/local/bin/phantomjs https://s3.amazonaws.com/circle-downloads/phantomjs-2.1.1
+    local BIN=/usr/local/bin/phantomjs
+
+    curl --output $BIN https://s3.amazonaws.com/circle-downloads/phantomjs-2.1.1
+    chmod +x $BIN
 }
