93a20d66c1137f986643f5ed8ae8377cee2d45b1
Katie <katie.ballinger@gmail.com>
2016-11-16 15:18:18 -0800

    build-targets ubuntu-14.04-XXL-enterprise ubuntu-14.04-enterprise

---
+++ circleci-provision-scripts/yarn.sh
@@ -0,0 +1,16 @@
+
+```#!/bin/bash
+
+function install_yarn() {
+    NODEJS_VERSION=$1
+    (cat <<'EOF'
+source ~/.circlerc
+nvm use $NODEJS_VERSION
+# tell npm to use known registrars for CA certs -
+# http://blog.npmjs.org/post/78085451721/npms-self-signed-certificate-is-no-more
+npm config set ca "" --global
+npm install -g yarn
+hash -r
+EOF
+    ) | as_user NODEJS_VERSION=$NODEJS_VERSION bash
+}
