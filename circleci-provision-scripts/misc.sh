bbe639d11df15b875efe55e21e9d602010140040
Marcus Stong <stongo@gmail.com>
2017-07-27 12:37:42 -0400

    enter tmp directory first in nsenter install

--- circleci-provision-scripts/misc.sh
+++ circleci-provision-scripts/misc.sh
@@ -124,8 +124,9 @@ function install_socat() {
 
 function install_nsenter() {
     apt-get install build-essential libncurses5-dev libslang2-dev gettext zlib1g-dev libselinux1-dev debhelper lsb-release pkg-config po-debconf autoconf automake autopoint libtool
+    pushd /tmp
     git clone git://git.kernel.org/pub/scm/utils/util-linux/util-linux.git util-linux
-    pushd util-linux/
+    cd util-linux/
     ./autogen.sh
     ./configure --without-python --disable-all-programs --enable-nsenter
     make
